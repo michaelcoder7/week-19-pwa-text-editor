2eb7f08d9c757eb746689e7b44aff9c327be61fe
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-31 12:09:09 +0100

    database updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
+// Imports the necessary function from the 'idb' library
 import { openDB } from "idb";
 
+// Initializes the database
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
@@ -15,19 +17,15 @@ const initdb = async () =>
 // Logic for Method that accepts some content and PUTs it to the database
 export const putDb = async (content) => {
   console.log("POST data to the database");
-  // Open a connection to the 'jate' database with value 1
+
   const jateDb = await openDB("jate", 1);
 
-  // Start a read-write transaction on the 'jate' object store
   const tx = jateDb.transaction("jate", "readwrite");
 
-  // Get the object store
   const store = tx.objectStore("jate");
 
-  // Use the .put() method to add content to the object store
   const request = store.put({ id: 1, content: content });
 
-  // Wait for the request to complete and get the result
   const result = await request;
   console.log("successfully saved data to database", result);
 };
@@ -36,22 +34,17 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log("GET request from database");
 
-  // Open a connection to the 'jate' database
   const jateDb = await openDB("jate", 1);
 
-  // Start a read-only transaction on the 'jate' object store
   const tx = jateDb.transaction("jate", "readonly");
 
-  // Get the object store
   const store = tx.objectStore("jate");
 
-  // Use the .get() method to retrieve data from the object store based on the id
   const request = store.get(1);
 
-  // Wait for the request to complete and return the value
   const result = await request;
   return result.value;
 };
 
-// Initialize the database when the module is loaded
+// Initializes the database when the module is loaded
 initdb();
